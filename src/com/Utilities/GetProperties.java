c064dff2aae2373404cc2808baf3b2c5352386d1
Couto, Sebastian Omar <sebastian.omar.couto@accenture.com>
2020-12-12 06:09:48 -0300

    GetProperties class added to the project

---
+++ src/com/Utilities/GetProperties.java
@@ -0,0 +1,24 @@
+package com.Utilities;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class GetProperties {
+
+	private Properties properties = new Properties();
+	
+	public GetProperties() {
+		try {
+			File src = new File ("./configuration/config.properties");
+			FileInputStream fis = new FileInputStream(src);
+			properties.load(fis);
+		} catch (Exception e) {
+			System.out.println("Has ocurred an exception: "+ e.getMessage());
+		}
+	}
+	
+	public String getString(String propName) {
+		return properties.getProperty(propName);
+	}
+}
