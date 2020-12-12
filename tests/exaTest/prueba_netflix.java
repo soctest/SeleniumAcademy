8e18bb2db0b4121e7807f90ada242ad924174f2f
Couto, Sebastian Omar <sebastian.omar.couto@accenture.com>
2020-12-12 13:27:04 -0300

    Cosmetic updates in current cases

--- tests/exaTest/prueba_netflix.java
+++ tests/exaTest/prueba_netflix.java
@@ -1,11 +1,12 @@
 package exaTest;
 
-import org.testng.Assert;
 import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
 import com.Utility.Helpers;
 import com.Utility.TestNGCreation;
 
@@ -17,22 +18,30 @@ public class prueba_netflix extends TestNGCreation {
 	@Test ()
 	@Parameters({"title"})
 	public void validarTituloTest(String netFlixTitle) {	
+		SoftAssert s_assert = new SoftAssert();
 		url = "https://www.netflix.com/";
 		driver.get(url);
 		String actualTitle = MainPage.pageTitle(driver, url);
-		Assert.assertTrue(actualTitle.contains(netFlixTitle), "The Netflix title is as expected!");
+		s_assert.assertTrue(actualTitle.contains(netFlixTitle), "The Netflix title is as expected!");
+		if (s_assert .equals(true)) {
+			System.out.println("The Netflix title is as expected!");
+		}
 	}
 	
 	@Test(dependsOnMethods = {"validarTituloTest"})
 	@Parameters({"title", "tagName"})
 	public void iniciarSesionPageTest(String netFlixTitle, String tagName) {
+		SoftAssert s_assert = new SoftAssert();
 		url = "https://www.netflix.com/";
 		driver.get(url);
 		driver.findElement(By.xpath(element.iniciarSesion)).click();
 		String actualTitle = MainPage.pageTitle(driver, url);
 		helper.sleepSeconds(5);
-		Assert.assertFalse((actualTitle.contains(netFlixTitle)), "The Netflix title has changed");
-			
+		s_assert.assertFalse((actualTitle.contains(netFlixTitle)), "The Netflix title has changed");
+		if (s_assert .equals(true)) {
+			System.out.println("The Netflix title has changed");
+		}
+		
 		List<WebElement> tagNameList = driver.findElements(By.tagName(tagName));
 		for (WebElement e : tagNameList) {
 			if (e.getText().contains("Sign In")) {
@@ -41,12 +50,16 @@ public class prueba_netflix extends TestNGCreation {
 			}
 		}
 		
-		Assert.assertTrue(driver.findElement(By.xpath(element.facebookSignIn)).isDisplayed(), "The 'Login with Facebook' text is displayed as expected!");
+		s_assert.assertTrue(driver.findElement(By.xpath(element.facebookSignIn)).isDisplayed(), "The 'Login with Facebook' text is displayed as expected!");
+		if (s_assert .equals(true)) {
+			System.out.println("The 'Login with Facebook' text is displayed as expected!");
+		}
 	}
 	
-	@Test(dependsOnMethods = {"validarTituloTest"})
+	@Test(dependsOnMethods = {"iniciarSesionPageTest"})
 	@Parameters({"netflixEmailWrong", "netflixEmailWell", "netflixPass", "netflixEmailErrMsg", "netflixPasswErrMsg"})
 	public void loginToNetflixErrorTest (String netflixEmailWrong, String netflixEmailWell,String netFlixPass, String netflixEmailErrMsg, String netflixPasswErrMsg) {
+		SoftAssert s_assert = new SoftAssert();
 		url = "https://www.netflix.com/";
 		driver.get(url);
 		driver.findElement(By.xpath(element.iniciarSesion)).click();
@@ -59,15 +72,24 @@ public class prueba_netflix extends TestNGCreation {
 		driver.findElement(By.xpath(element.netflixSignInBtn)).click();
 		
 		String netfEmailErrMsg = MainPage.loginEmailErrMsg(driver, url, driver.findElement(By.xpath(element.netFlixEmailErrMsglocator)));
-		Assert.assertTrue(netfEmailErrMsg.contains(netflixEmailWrong), "The error message '"+ netflixEmailErrMsg +"' is displayed as expected!");
-			
+		s_assert.assertTrue(netfEmailErrMsg.contains(netflixEmailWrong), "The Email error message '"+ netflixEmailErrMsg +"' is displayed as expected!");
+		
+		if (s_assert .equals(true)) {
+			System.out.println("The Email error message '"+ netflixEmailErrMsg +"' is displayed as expected!");
+		}
+		
+		helper.sleepSeconds(2);
+		driver.findElement(By.id(element.netflixEmail)).click();
 		helper.sleepSeconds(2);
 		driver.findElement(By.id(element.netflixEmail)).clear();
 		driver.findElement(By.id(element.netflixEmail)).sendKeys(netflixEmailWell);
 		driver.findElement(By.xpath(element.netflixSignInBtn)).click();
 		
 		String netPasswfErrMsg = MainPage.loginPasswErrMsg(driver, url, driver.findElement(By.xpath(element.netFlixPasswErrMsglocator)));
-		Assert.assertTrue(netPasswfErrMsg.contains(netflixPasswErrMsg), "The error message '"+ netflixPasswErrMsg +"' is displayed as expected!");
+		s_assert.assertTrue(netPasswfErrMsg.contains(netflixPasswErrMsg), "The Password error message '"+ netflixPasswErrMsg +"' is displayed as expected!");
+		if (s_assert .equals(true)) {
+			System.out.println("The Password error message '"+ netflixPasswErrMsg +"' is displayed as expected!");
+		}
 	}
 	
 	
